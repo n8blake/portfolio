f54c061609a571178e34dd892ae8baa4bf56bb90
Nathan Blake <developer@blake-ink.com>
2021-02-07 16:48:16 -0800

    keep working on layout

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
 	</header>
 	<main>
 		<nav>
-			<a href="#">About</a>
-			<a href="#">Work</a>
+			<a href="#" class="nav-link active">About</a>
+			<a href="#" class="nav-link">Work</a>
 		</nav>
-		<section>
+		<section class="section active">
 			<img src="self_portrait_sm.png" />
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -37,10 +37,18 @@
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</section>
+		<section class="section">
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+				<li>Item 4</li>
+			</ul>
+		</section>
 	</main>
 	<footer>
 		<div class="footer-text">Find me on</div>
-		<div><a href="#" class="github">GitHub</a> <a href="#" class="twitter">Twitter</a> <a href="#" class="instagram">Instagram</a> <a href="#" class="youtube">YouTube</a></div>
+		<div class="footer-links"><a href="#" class="github">GitHub</a> <a href="#" class="twitter">Twitter</a> <a href="#" class="instagram">Instagram</a> <a href="#" class="youtube">YouTube</a></div>
 	</footer>
 </body>
 </html>
