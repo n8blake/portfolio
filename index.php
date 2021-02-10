59c79eb30d9f0a80770d62cce4b431711c6a5d91
Nathan Blake <developer@blake-ink.com>
2021-02-09 17:44:22 -0800

    update base styles

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<title>n8blake</title>
 	<link rel="stylesheet" type="text/css" href="reset.css">
 	<link rel="stylesheet" type="text/css" href="base_style.css">
+	<link rel="stylesheet" type="text/css" href="styles_sm.css">
 </head>
 <body>
 	<header>
@@ -20,10 +21,10 @@
 	</header>
 	<main>
 		<nav>
-			<a href="#" class="nav-link active">About</a>
-			<a href="#" class="nav-link">Work</a>
+			<a href="#" class="nav-link ">about</a>
+			<a href="#" class="nav-link active">projects</a>
 		</nav>
-		<section class="section active">
+		<section class="section ">
 			<img src="self_portrait_sm.png" />
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -38,18 +39,49 @@
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</section>
-		<section class="section">
+		<section class="section active">
 			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-				<li>Item 3</li>
-				<li>Item 4</li>
+				<li class="portfolio-item">
+					<img src="./photos/ROR_sm.jpg">
+					<h2>Thing</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</li>
+				<li class="portfolio-item">
+					<img src="./photos/ROR_sm.jpg">
+					<h2>Thing</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</li>
+				<li class="portfolio-item">
+					<img src="./photos/ROR_sm.jpg">
+					<h2>Thing</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</li>
+
 			</ul>
 		</section>
 	</main>
 	<footer>
 		<div class="footer-text">Find me on</div>
-		<div class="footer-links"><a href="#" class="github">GitHub</a> <a href="#" class="twitter">Twitter</a> <a href="#" class="instagram">Instagram</a> <a href="#" class="youtube">YouTube</a></div>
+		<div class="footer-links">
+			<a href="https://github.com/n8blake" target="_blank" class="github">GitHub</a> 
+			<a href="https://twitter.com/n8blake" target="_blank" class="twitter">Twitter</a> 
+			<a href="https://www.instagram.com/n8blake/" target="_blank" class="instagram">Instagram</a> 
+			<a href="https://www.youtube.com/user/n8blake" target="_blank" class="youtube">YouTube</a></div>
 	</footer>
 </body>
 </html>
