7f819e75b1617297614edeab8359838d69613a46
Nathan Blake <developer@blake-ink.com>
2021-02-11 09:24:02 -0800

    add js for tab switching, continue work on projects css

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
 	</header>
 	<main>
 		<nav>
-			<a href="#" class="nav-link ">about</a>
-			<a href="#" class="nav-link active">projects</a>
+			<a href="#" id="about-link" data-target="about" class="nav-link " onclick="activate(this)">about</a>
+			<a href="#" id="projects-link" data-target="projects" class="nav-link active" onclick="activate(this)">projects</a>
 		</nav>
-		<section class="section ">
+		<section class="section " id="about-section">
 			<img src="self_portrait_sm.png" />
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -39,24 +39,36 @@
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</section>
-		<section class="section active">
+		<section class="section active" id="projects-section">
 			<ul>
 				<li class="portfolio-item">
 					<img src="./photos/ROR_sm.jpg">
-					<h2>Project 1</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+					<div class="item-text">
+						<h2 class="item-title">Project 1</h2>
+						<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+						<a href="#">Demo</a>
+					</div>
 				</li>
+				
 				<li class="portfolio-item">
 					<img src="./photos/ROR_sm.jpg">
-					<h2>Project 2</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore.</p>
+					<div class="item-text">
+						<h2 class="item-title">Project 2</h2>
+						<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+					</div>
 				</li>
+				
 				<li class="portfolio-item">
 					<img src="./photos/ROR_sm.jpg">
-					<h2>Project 3</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					<div class="item-text">
+						<h2 class="item-title">Project 3</h2>
+						<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
 				</li>
 
 			</ul>
@@ -70,5 +82,6 @@
 			<a href="https://www.instagram.com/n8blake/" target="_blank" class="instagram">Instagram</a> 
 			<a href="https://www.youtube.com/user/n8blake" target="_blank" class="youtube">YouTube</a></div>
 	</footer>
+	<script type="text/javascript" src="portfolio.js"></script>
 </body>
 </html>
---
+++ portfolio.js
@@ -0,0 +1,14 @@
+function activate(element){
+	//console.log(element);
+	let target = element.getAttribute('data-target');
+	targetSection = document.getElementById(target + '-section');
+	if(target === 'about'){
+		document.getElementById('projects-link').classList.remove('active');
+		document.getElementById('projects-section').classList.remove('active');
+	} else {
+		document.getElementById('about-link').classList.remove('active');
+		document.getElementById('about-section').classList.remove('active');
+	}
+	element.classList.add("active");
+	targetSection.classList.add("active");
+}
